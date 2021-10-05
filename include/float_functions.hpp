dce5073685a7b703c0a5a0ca08a1c92cabe14ac8
Armend Veseli <armend.veseli@yahoo.com>
2021-10-05 17:52:41 -0400

    Update float_functions.hpp

--- include/float_functions.hpp
+++ include/float_functions.hpp
@@ -91,6 +91,6 @@ namespace simple_physics
     float luminosity_distance(float luminosity, float redshift, float angular_diameter_distance);
     float wave_intensity(float power, float area);
     float orbital_period(float mass_of_orbited_body);
-	float rocket_delta_velocity(float specific_impulse, float gravitational_acceleration, float initial_mass, float final_mass);
-	float doppler_effect(float sound_wave_speed, float observer_velocity, float source_velocity, float actual_sound_wave_frequency);
+    float rocket_delta_velocity(float specific_impulse, float gravitational_acceleration, float initial_mass, float final_mass);
+    float doppler_effect(float sound_wave_speed, float observer_velocity, float source_velocity, float actual_sound_wave_frequency);
 };
