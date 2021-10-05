5e0f10a711dd66244e102755f07f41fa9672e0f4
Armend Veseli <armend.veseli@yahoo.com>
2021-10-05 15:50:08 -0400

    Update float_functions.cpp

--- src/float_functions.cpp
+++ src/float_functions.cpp
@@ -529,4 +529,9 @@ namespace simple_physics
 	{
 		return (4 * M_PI * M_PI) / (NEWTONS_GRAVITATIONAL_CONSTANT * mass_of_orbited_body);
 	}
+	
+	float rocket_delta_velocity(float specific_impulse, float gravitational_acceleration, float initial_mass, float final_mass)
+	{
+		return specific_impulse * gravitational_acceleration * log(initial_mass / final_mass);
+	}	
 }
