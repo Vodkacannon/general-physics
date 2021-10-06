9cd5d02134042ea66ede17969500912bfa51c573
Armend Veseli <armend.veseli@yahoo.com>
2021-10-06 19:23:37 -0400

    Update vec_2f.hpp

--- src/vec_2f.hpp
+++ src/vec_2f.hpp
@@ -4,11 +4,6 @@ namespace simple_physics
 {
   vec_2f::vec_2f(float x, float y)
   {
-    vec_2f result;
-  
-    result.x = x;
-    result.y = y;
-  
-    return result;
+    return {x, y};
   }
 }
