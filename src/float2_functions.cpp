933c1824b6c10c19b1202bc709b2f7ed8d302adb
Armend Veseli <armend.veseli@yahoo.com>
2021-10-06 16:49:07 -0400

    Update float2_functions.cpp

--- src/float2_functions.cpp
+++ src/float2_functions.cpp
@@ -2,17 +2,17 @@
 
 namespace simple_physics
 {
-	float vec_2f_speed(vec2f distance, float time)
+	float vec_2f_speed(const vec2f distance, const float time)
 	{
 		return float2(distance.x / time, distance.y / time);
 	}
 	
-	float vec_2f_distance(vec2f speed, float time)
+	float vec_2f_distance(const vec2f speed, const float time)
 	{
 		return vec2f(speed.x / time, speed.y / time, speed.z / time);
 	}
 	
-	float vec_2f_time(vec2f distance, vec2f velocity)
+	float vec_2f_time(const vec2f distance, const vec2f velocity)
 	{
 		return vec2f(distance.x / velocity.x, distance.y / velocity.y);
 	}
