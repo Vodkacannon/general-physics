ff3fbcdcd66fa0baa7e33d8a544f907635a5ac5e
Armend Veseli <armend.veseli@yahoo.com>
2021-10-06 09:48:02 -0400

    Update momentum_3f.cpp

--- src/momentum_3f.cpp
+++ src/momentum_3f.cpp
@@ -1,9 +1,7 @@
 #include "float_types.hpp"
 #include "vec_3f.hpp"
 
-namespace simple_physics {
-    struct momentum_3f {
-        float mass;
-        vec_3f velocity;
-    };
-}
+namespace simple_physics
+{
+   
+};
