08109bc506607919db1eb94ac26ec84a7a005ac0
Armend Veseli <armend.veseli@yahoo.com>
2021-10-13 18:59:00 -0400

    Update vec_2f.hpp

--- src/vec_2f.hpp
+++ src/vec_2f.hpp
@@ -7,7 +7,7 @@ namespace simple_physics
     return {x, y};
   }
   
-  vec_2f::add(const vec2f& vector_1, const vec2f& vector_2)
+  vec_2f vec_2f::add(const vec2f& vector_1, const vec2f& vector_2)
   {
     return {vector_1.x + vector_2.x, vector_2.x + vector_2.y};
   }
