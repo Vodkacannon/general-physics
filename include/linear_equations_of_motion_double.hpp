261e3b60e2ea48b418dc126582a91a4016868dfe
Armend Veseli <armend.veseli@yahoo.com>
2021-10-21 23:17:01 -0400

    Update linear_equations_of_motion_double.hpp

--- include/linear_equations_of_motion_double.hpp
+++ include/linear_equations_of_motion_double.hpp
@@ -1,4 +1,12 @@
 namespace simple_physics
 {
-
+	double speed(double distance, double time);
+	double velocity(double displacement, double time);
+	double displacement(double velocity, double time);
+	double distance(double speed, double time);
+	double time(double distance, double velocity);
+	double displacement_2(double velocity_1, double time, double acceleration);
+	double displacement_3(double velocity_1, double velocity_2, double acceleration);
+	double displacement_4(double velocity_1, double velocity_2, double time);
+	double acceleration(double velocity_2, double velocity_1, double time_2, double time_1);
 }
