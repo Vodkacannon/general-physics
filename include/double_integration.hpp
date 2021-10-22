d0bc7a01e7bf2964a1ce4270af221820d3401225
Armend Veseli <armend.veseli@yahoo.com>
2021-10-22 14:21:20 -0400

    Update double_integration.hpp

--- include/double_integration.hpp
+++ include/double_integration.hpp
@@ -1,8 +1,9 @@
 namespace simple_physics
 {
-	double integrate_jerk_to_acceleration(double jerk, double delta_time);
-	double integrate_acceleration_to_velocity(double acceleration, double delta_time);
-	double integrate_velocity_to_position(double velocity, double delta_time);
-	double integrate_jerk_to_position(double jerk, double delta_time);
-	double integrate_acceleration_to_position(double acceleration, double delta_time);
+    double integrate_jerk_to_acceleration(double jerk, double delta_time);
+    double integrate_jerk_to_velocity(double jerk, double delta_time);
+    double integrate_jerk_to_position(double jerk, double delta_time);
+    double integrate_acceleration_to_velocity(double acceleration, double delta_time);
+    double integrate_acceleration_to_position(double acceleration, double delta_time);
+    double integrate_velocity_to_position(double velocity, double delta_time);
 }
