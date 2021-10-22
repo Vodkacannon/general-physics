53c597b4e9c54a82ad682291bd649ddede729d8b
Armend Veseli <armend.veseli@yahoo.com>
2021-10-21 23:13:49 -0400

    Update float_integration.hpp

--- include/float_integration.hpp
+++ include/float_integration.hpp
@@ -5,5 +5,4 @@ namespace simple_physics
     float integrate_velocity_to_position(float velocity, float delta_time);
     float integrate_jerk_to_position(float jerk, float delta_time);
     float integrate_acceleration_to_position(float acceleration, float delta_time);
-	float is_total_energy_zero(float kinetic_energy, float potential_energy, float floating_point_tolerance);
 }
