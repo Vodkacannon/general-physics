1a2e2681a9d7c624607b14c7253b2b559daf4f64
Armend Veseli <armend.veseli@yahoo.com>
2021-10-22 14:20:24 -0400

    Update float_integration.hpp

--- include/float_integration.hpp
+++ include/float_integration.hpp
@@ -1,8 +1,9 @@
 namespace simple_physics
 {
     float integrate_jerk_to_acceleration(float jerk, float delta_time);
-    float integrate_acceleration_to_velocity(float acceleration, float delta_time);
-    float integrate_velocity_to_position(float velocity, float delta_time);
+    float itegrate_jerk_to_velocity(float jerk, float delta_time);
     float integrate_jerk_to_position(float jerk, float delta_time);
+    float integrate_acceleration_to_velocity(float acceleration, float delta_time);
     float integrate_acceleration_to_position(float acceleration, float delta_time);
+    float integrate_velocity_to_position(float velocity, float delta_time);
 }
