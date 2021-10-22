50068a7e20419abce714885dd10deb73a29b53d4
Armend Veseli <armend.veseli@yahoo.com>
2021-10-22 14:43:35 -0400

    Update vec_2f_functions.cpp

--- src/vec_2f_functions.cpp
+++ src/vec_2f_functions.cpp
@@ -29,6 +29,6 @@ namespace simple_physics
   
   	vec_2f vec_2f::add(const vec2f& vector_1, const vec2f& vector_2)
   	{
-    	return {vector_1.x + vector_2.x, vector_2.x + vector_2.y};
+    		return {vector_1.x + vector_2.x, vector_2.x + vector_2.y};
   	}
 }
