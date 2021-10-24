bacc720d3aa169f17fe01a6ac98cb605445da533
Armend Veseli <armend.veseli@yahoo.com>
2021-10-24 17:13:23 -0400

    Update float_functions.cpp

--- src/float_functions.cpp
+++ src/float_functions.cpp
@@ -453,30 +453,7 @@ namespace simple_physics
 		return (COULOMBS_CONSTANT * charge_1 * charge_2) / (distance_between_charges * distance_between_charges);
 	}
 	
-	float integrate_jerk_to_acceleration(float jerk, float delta_time)
-	{
-		return jerk * delta_time;
-	}
 	
-    	float integrate_acceleration_to_velocity(float acceleration, float delta_time)
-	{
-		return acceleration * delta_time;
-	}
-	
-    	float integrate_velocity_to_position(float velocity, float delta_time)
-	{
-		return velocity * delta_time;
-	}
-	
-    	float integrate_jerk_to_position(float jerk, float delta_time)
-	{
-		return integrate_velocity_to_position(integrate_acceleration_to_velocity(integrate_jerk_to_acceleration(float jerk, float delta_time), float delta_time), delta_time);
-	}
-	
-    	float integrate_acceleration_to_position(float acceleration, float delta_time)
-	{
-		return integrate_velocity_to_position(integrate_acceleration_to_velocity(float acceleration, float delta_time), float delta_time);
-	}
 	
 	float is_total_energy_zero(float kinetic_energy, float potential_energy, float floating_point_tolerance)
 	{
