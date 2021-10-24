d6972c4c6d7137ae9908094c507d968abbdbab8f
Armend Veseli <armend.veseli@yahoo.com>
2021-10-24 17:25:02 -0400

    Update float_electrical_functions.cpp

--- src/float_electrical_functions.cpp
+++ src/float_electrical_functions.cpp
@@ -1,5 +1,30 @@
 namespace simple_physics
 {
+  float charge(float voltage, float time)
+	{
+		return voltage * time;
+	}
+
+	float electric_energy(float voltage, float current, float time)
+	{
+		return voltage * current * time;
+	}
+
+	float electric_energy(float voltage, float charge)
+	{
+		return voltage * charge;
+	}
+
+	float electric_power(float voltage, float current)
+	{
+		return voltage * current;
+	}
+	
+	float electric_power_2(float current, float resistance)
+	{
+		return current * resistance * resistance;
+	}
+  
   capacitor_energy(float quantity_of_charge_separated, float applied_potential_difference)
   {
     return (1 / 2) * (quantity_of_charge_separated * quantity_of_charge_separated) / applied_potential_difference;
