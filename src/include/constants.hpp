bffa4ce265b1d68e9e05e1fd9716bea9d49f86c0
Armend Veseli <armend.veseli@yahoo.com>
2021-10-27 15:05:43 -0400

    Create constants.hpp

---
+++ src/include/constants.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <math.h>
+
+namespace simple_physics 
+{
+    const float NEWTONS_GRAVITATIONAL_CONSTANT = 6.67430e-11;
+    const float SPEED_OF_LIGHT = 299792458;
+    const float EARTHS_GRAVITATIONAL_CONSTANT = 9.80665;
+    const float COULOMBS_CONSTANT = 8.9875517923e9;
+    const float PLANCKS_CONSTANT = 6.62607004e-34;
+    const float REDUCED_PLANCKS_CONSTANT = PLANCKS_CONSTANT / (2 * M_PI);
+    const float BOLTZMANS_CONSTANT = 1.38064852e-23;
+    const float IDEAL_GAS_CONSTANT = 8.31446261815324;
+    const float RAYLEIGHS_CRITERION_CONSTANT = 1.21966989;
+    const float VACUUM_PERMITTIVITY_CONSTANT = 8.8541878128e−12;
+    const float STEFAN_BOLTZMAN_CONSTANT = 5.670374419e-8;
+};
