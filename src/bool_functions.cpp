db7d83e18b870500088536f48dd74a28239dc047
Armend Veseli <armend.veseli@yahoo.com>
2021-10-27 17:50:12 -0400

    Update bool_functions.cpp

--- src/bool_functions.cpp
+++ src/bool_functions.cpp
@@ -1,1 +1,12 @@
+namespace simple_physics
+{
+    bool are_forces_in_equilibrium(const std::vector<std::pair<float_types::mass, float_types::acceleration>> &forces)
+    {
+		if(sum_of_forces(forces) == 0)
+		{
+			return true;
+		}
 
+		return false;
+	}
+}
