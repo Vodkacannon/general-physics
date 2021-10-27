3d2225fad60260d96537a825ed25c4671c0bbc7d
Armend Veseli <armend.veseli@yahoo.com>
2021-10-27 17:52:10 -0400

    Update bool_functions.cpp

--- src/bool_functions.cpp
+++ src/bool_functions.cpp
@@ -1,12 +1,22 @@
 namespace simple_physics
 {
-    bool are_forces_in_equilibrium(const std::vector<std::pair<float_types::mass, float_types::acceleration>> &forces)
-    {
+	bool are_forces_in_equilibrium(const std::vector<std::pair<float_types::mass, float_types::acceleration>> &forces)
+	{
 		if(sum_of_forces(forces) == 0)
 		{
 			return true;
 		}
 
+		return false;
+	}
+	
+	bool is_total_energy_zero(float kinetic_energy, float potential_energy, float floating_point_tolerance)
+	{
+		if(fabs(kinetic_energy + potential_energy) <= floating_point_tolerance)
+		{
+			return true;
+		}
+		
 		return false;
 	}
 }
