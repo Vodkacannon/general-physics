2c77590275713d9f16adb95155af7dd4ca538661
Armend Veseli <armend.veseli@yahoo.com>
2021-11-02 00:26:00 -0400

    Update double_dynamics.cpp

--- src/double_dynamics.cpp
+++ src/double_dynamics.cpp
@@ -1,4 +1,32 @@
 namespace simple_physics
 {
+    	double momentum(double mass, double velocity)
+    	{
+        	return mass * velocity;
+    	}
 
+    	double force(double mass, double acceleration)
+    	{
+        	return mass * acceleration;
+    	}
+
+    	double pressure(double force, double area)
+    	{
+        	return force / area;
+    	}
+	
+    	double density(double mass, double volume)
+    	{
+        	return mass / volume;
+    	}
+
+    	double impulse(double force, double time)
+    	{
+        	return force * time;
+    	}
+
+    	double work(double force, double distance)
+    	{
+        	return force * distance;
+    	}
 }
