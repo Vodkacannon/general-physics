ab395b6917287cda0fc3b9c641c3ffb499c7297c
Armend Veseli <armend.veseli@yahoo.com>
2021-11-02 18:51:49 -0400

    Update float_astrophysics.cpp

--- src/float_astrophysics.cpp
+++ src/float_astrophysics.cpp
@@ -8,5 +8,15 @@ namespace simple_physics
 	float rocket_delta_velocity(float specific_impulse, float gravitational_acceleration, float initial_mass, float final_mass)
 	{
 		return specific_impulse * gravitational_acceleration * log(initial_mass / final_mass);
-	}  
+	}
+	
+	float newtons_gravitational_force(float mass_1, float mass_2, float radius_between_masses)
+	{
+		return NEWTONS_GRAVITATIONAL_CONSTANT * mass_1 * mass_2 / (radius_between_masses * radius_between_masses);
+	}
+
+	float escape_speed(float mass, float radius)
+	{
+		return sqrtf((2 * NEWTONS_GRAVITATIONAL_CONSTANT * mass) / radius);
+	}
 }
