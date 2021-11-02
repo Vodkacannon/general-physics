23a31db2b936d7cfe9a7fc59d0472046b0902574
Armend Veseli <armend.veseli@yahoo.com>
2021-11-02 19:05:50 -0400

    Update nuclear_functions.cpp

--- src/nuclear_functions.cpp
+++ src/nuclear_functions.cpp
@@ -1,5 +1,17 @@
+#include <math.h>
+
 namespace simple_physics
 {
+        float s_factor(float energy, float cross_section_of_energy, float sommerfeld_parameter)
+        {
+                return (energy * cross_section_of_energy) / expf(-2 * M_PI * cross_section); 
+        }
+        
+        float excess_mass(float total_rest_mass_of_fission_products, float mass_of_origional_fuel_nucleus)
+        {
+                return total_rest_mass_of_fission_products * mass_of_origional_fuel_nucleus;
+        }
+        
         float prompt_neutron_mean_free_path(float nuclear_number_density, float total_interaction_cross_section)
         {
                 return 1 / (nuclear_number_density * total_interaction_cross_section);
