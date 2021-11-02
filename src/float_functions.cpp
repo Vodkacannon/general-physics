dbb5852dc668adff6cbc9e0ea6d4b50a33436575
Armend Veseli <armend.veseli@yahoo.com>
2021-11-02 00:23:30 -0400

    Update float_functions.cpp

--- src/float_functions.cpp
+++ src/float_functions.cpp
@@ -7,36 +7,6 @@
 
 namespace simple_physics
 {
-	float momentum(float mass, float velocity)
-	{
-		return mass * velocity;
-	}
-
-	float force(float mass, float acceleration)
-	{
-		return mass * acceleration;
-	}
-
-	float pressure(float force, float area)
-	{
-		return force / area;
-	}
-
-	float density(float mass, float volume)
-	{
-		return mass / volume;
-	}
-
-	float impulse(float force, float time)
-	{
-		return force * time;
-	}
-
-	float work(float force, float distance)
-	{
-		return force * distance;
-	}
-
 	float final_free_fall_velocity(float acceleration_of_gravity, float initial_height)
 	{
 		return sqrtf(2 * acceleration_of_gravity * initial_height);
