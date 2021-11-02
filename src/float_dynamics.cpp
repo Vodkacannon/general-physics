014c74ac64a7dd8c273cc12a8b03ca318e549e9b
Armend Veseli <armend.veseli@yahoo.com>
2021-11-02 00:24:02 -0400

    Update float_dynamics.cpp

--- src/float_dynamics.cpp
+++ src/float_dynamics.cpp
@@ -1,4 +1,32 @@
 namespace simple_physics
 {
-  
+        float momentum(float mass, float velocity)
+	{
+		return mass * velocity;
+	}
+
+	float force(float mass, float acceleration)
+	{
+		return mass * acceleration;
+	}
+
+	float pressure(float force, float area)
+	{
+		return force / area;
+	}
+
+	float density(float mass, float volume)
+	{
+		return mass / volume;
+	}
+
+	float impulse(float force, float time)
+	{
+		return force * time;
+	}
+
+	float work(float force, float distance)
+	{
+		return force * distance;
+	}
 }
