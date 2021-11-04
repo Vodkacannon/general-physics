361357cf486f3e3ce86a9f578ab7d3d2a63f3bae
Armend Veseli <armend.veseli@yahoo.com>
2021-11-03 23:38:13 -0400

    Update float_functions.cpp

--- src/float_functions.cpp
+++ src/float_functions.cpp
@@ -165,26 +165,6 @@ namespace simple_physics
 	{
 		return (mass * drag_coefficient) / cross_sectional_area;
 	}
-
-	float maluses_law(float initial_intensity, float angle_between_polarizer_and_initial_polarization_direction)
-	{
-		return initial_intensity * cos(angle_between_polarizer_and_initial_polarization_direction) * cos(angle_between_polarizer_and_initial_polarization_direction);
-	}
-	
-	float rayleigh_criterion(float light_wavelength, float lens_aperture_diameter)
-	{
-		return RAYLEIGHS_CRITERION_CONSTANT * (light_wavelength / lens_aperture_diameter);
-	}
-	
-	float lateral_resolution(float light_wavelength, float half_angle_of_light, float index_of_refraction)
-	{
-		return (RAYLEIGHS_CRITERION_CONSTANT * light_wavelength) / (2 * index_of_refraction * sin(half_angle_of_light));
-	}
-									    
-	float luminosity_distance(float luminosity, float redshift, float angular_diameter_distance)
-	{
-		return luminosity / (4 * M_PI * (1 + redshift) * (1 + redshift) * angular_diameter_distance); 	
-	}
 	
 	float wave_intensity(float power, float area)
 	{
