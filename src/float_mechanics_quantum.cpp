e615e736c10b4d904b94b959b8735e460ab713c3
Armend Veseli <armend.veseli@yahoo.com>
2021-11-03 23:35:32 -0400

    Update float_mechanics_quantum.cpp

--- src/float_mechanics_quantum.cpp
+++ src/float_mechanics_quantum.cpp
@@ -1,4 +1,22 @@
 namespace simple_physics
 {
-  
+	float photon_energy(float frequency)
+	{
+		return PLANCKS_CONSTANT * frequency;
+	}
+
+	float photon_momentum(float wavelength)
+	{
+		return PLANCKS_CONSTANT * wavelength;
+	}
+
+	float heisenberg_momentum_uncertainty(float max_position, float min_position)
+	{
+		return REDUCED_PLANCKS_CONSTANT / (2 * (max_position - min_position));
+	}
+
+	float heisenberg_position_uncertainty(float max_momentum, float min_momentum)
+	{
+		return REDUCED_PLANCKS_CONSTANT / (2 * (max_momentum - min_momentum));
+	}  
 }
