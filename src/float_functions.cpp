f7d8b8e8f45fdbd5e237589c2f6142d4ee2059cb
Armend Veseli <armend.veseli@yahoo.com>
2021-11-06 22:08:44 -0400

    Update float_functions.cpp

--- src/float_functions.cpp
+++ src/float_functions.cpp
@@ -45,29 +45,6 @@ namespace simple_physics
 
 		return mass_moment_sum / sum_of_masses;
 	}
-
-	//Calculate the number of significant figures of a float.
-	float significant_figures(float value)
-	{
-		std::string value_as_string = std::to_string(value);
-
-		unsigned int string_length = value_as_string.length();
-		unsigned short significant_figure_count = 0;
-
-		for(unsigned int i = 0; i < string_length; i++)
-		{
-			if(value_as_string[0] == '0')
-			{
-				continue;
-			}
-			else
-			{
-				significant_figure_count++; 
-			}
-		}
-
-		return significant_figure_count;
-	}
 	
 	float power(float energy, float time)
 	{
