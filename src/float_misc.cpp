f66a282c58754dfb5c94d6871e9cffbb8be4a767
Armend Veseli <armend.veseli@yahoo.com>
2021-11-07 11:57:38 -0500

    Update float_misc.cpp

--- src/float_misc.cpp
+++ src/float_misc.cpp
@@ -22,4 +22,43 @@ namespace simple_physics
 
 		return significant_figure_count;
 	}
+	
+	float sum_of_momenta(const std::vector<std::pair<mass, velocity>> &momenta)
+	{
+		float sum_of_momenta = 0.0;
+		
+		for(const auto& momentum : momenta)
+		{
+			sum_of_momenta += momentum.first * momentum.second;
+		}
+
+		return sum_of_momenta;
+	}
+
+	float sum_of_forces(const std::vector<std::pair<mass, acceleration>> &forces)
+	{
+		float sum_of_forces = 0.0;
+		
+		for(const auto& force : forces)
+		{
+			sum_of_forces += force.first * force.second;
+		}
+
+		return sum_of_forces;
+	}
+
+	float center_of_mass(const std::vector<std::pair<float_types::mass, float_types::radius_from_zero>> &mass_moments)
+	{
+		float sum_of_masses = 0.0;
+		float mass_moment_sum = 0.0;
+		unsigned int mass_moments_size = mass_moments.size();
+
+		for(unsigned int i = 0; i < mass_moments_size; i++)
+		{
+			mass_moment_sum += mass_moments[i].first * mass_moments[i].second;
+			sum_of_masses += mass_moments[i].first;
+		}
+
+		return mass_moment_sum / sum_of_masses;
+	}
 }
