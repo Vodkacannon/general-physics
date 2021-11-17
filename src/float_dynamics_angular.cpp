94a7612d2b563d2decd0663da96d06cdc57232d8
Armend Veseli <armend.veseli@yahoo.com>
2021-11-16 21:48:56 -0500

    Update float_dynamics_angular.cpp

--- src/float_dynamics_angular.cpp
+++ src/float_dynamics_angular.cpp
@@ -1,7 +1,12 @@
 namespace simple_physics
 {
-  float period_of_spring_with_mass(float mass, float spring_constant)
+	float period_of_spring_with_mass(float mass, float spring_constant)
 	{
 		2 * M_PI * sqrtf(mass / spring_constant);
+	}
+	
+	float pendulum_potential_energy(float mass, float gravitational_acceleration, float pendulum_length, float angle)
+	{
+		return mass * gravitational_acceleration * pendulum_length * (1 - cos(angle));
 	}
 }
