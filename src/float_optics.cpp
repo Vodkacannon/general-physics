f3518e513f8faa40345edcedf35c69f52cdc7039
Armend Veseli <armend.veseli@yahoo.com>
2021-11-16 21:59:41 -0500

    Update float_optics.cpp

--- src/float_optics.cpp
+++ src/float_optics.cpp
@@ -4,7 +4,7 @@ namespace simple_physics
 {
 	float maluses_law(float initial_intensity, float angle_between_polarizer_and_initial_polarization_direction)
 	{
-		return initial_intensity * cos(angle_between_polarizer_and_initial_polarization_direction) * cos(angle_between_polarizer_and_initial_polarization_direction);
+		return initial_intensity * cosf(angle_between_polarizer_and_initial_polarization_direction) * cosf(angle_between_polarizer_and_initial_polarization_direction);
 	}
 	
 	float rayleigh_criterion(float light_wavelength, float lens_aperture_diameter)
@@ -14,7 +14,7 @@ namespace simple_physics
 	
 	float lateral_resolution(float light_wavelength, float half_angle_of_light, float index_of_refraction)
 	{
-		return (RAYLEIGHS_CRITERION_CONSTANT * light_wavelength) / (2 * index_of_refraction * sin(half_angle_of_light));
+		return (RAYLEIGHS_CRITERION_CONSTANT * light_wavelength) / (2 * index_of_refraction * sinf(half_angle_of_light));
 	}
 									    
 	float luminosity_distance(float luminosity, float redshift, float angular_diameter_distance)
