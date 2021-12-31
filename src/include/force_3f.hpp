cb69583c5fcf0dcc743e392daa7368c8b6cc718d
Armend Veseli <armend.veseli@yahoo.com>
2021-12-31 11:20:19 -0500

    Update force_3f.hpp

--- src/include/force_3f.hpp
+++ src/include/force_3f.hpp
@@ -1,7 +1,7 @@
 #include "float_types.hpp"
 #include "vec_3f.hpp"
 
-namespace simple_physics {
+namespace general_physics {
     struct force_3f {
         float mass;
         vec_3f acceleration;
