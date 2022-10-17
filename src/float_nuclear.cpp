3fa6ed22de1a7681d7e4a64882b53157b532b5d4
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-17 18:03:43 -0400

    Update float_nuclear.cpp

--- src/float_nuclear.cpp
+++ src/float_nuclear.cpp
@@ -1,12 +1,13 @@
 #pragma once
 
+#include <iostream>
 #include <math.h>
 
 namespace simple_physics
 {
         float s_factor(float energy, float cross_section_of_energy, float sommerfeld_parameter)
         {
-                return (energy * cross_section_of_energy) / expf(-2 * M_PI * cross_section); 
+                return (energy * cross_section_of_energy) / expf(-2.0f * M_PI * cross_section); 
         }
         
         float excess_mass(float total_rest_mass_of_fission_products, float mass_of_origional_fuel_nucleus)
@@ -16,13 +17,14 @@ namespace simple_physics
         
         float prompt_neutron_mean_free_path(float nuclear_number_density, float total_interaction_cross_section)
         {
-                return 1 / (nuclear_number_density * total_interaction_cross_section);
+                return 1.0f / (nuclear_number_density * total_interaction_cross_section);
         }
         
         //This may be a closely guarded secret.
-        float prompt_criticality_reaction_growth_rate(float initial_growth_rate, neutron_time_to_cause_fission)
+        float prompt_criticality_reaction_growth_rate(float initial_growth_rate, float neutron_time_to_cause_fission)
         {
-                return 0;
+                std::cout << "prompt_criticality_reaction_growth_rate() is unknown." << std::endl;
+                return 0.0f;
         }
         
         float mass_difference_of_nucleus(float atomic_number, float mass_number, float nucleus_mass)
@@ -37,7 +39,7 @@ namespace simple_physics
              
         float half_life(float decay_constant) 
         {
-             return logf(2) / decay_constant; 
+             return logf(2.0f) / decay_constant; 
         }
         
         float radiation_particles_remaining(float decay_constant, float radiation_particles_time_0, float current_time) 
