9589a2ad5a5b310e9cf34ad074b16f15e527a7df
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-20 16:32:24 -0400

    Update float_fluid_mechanics.h

--- src/include/float_fluid_mechanics.h
+++ src/include/float_fluid_mechanics.h
@@ -5,5 +5,9 @@ namespace general_physics
         
         //Pipe volume flow rate.
 	float hagen_poiseuille_law(float pressure_difference, float pipe_radius, float fluid_viscosity, float pipe_length);
-	float specific_weight(float density_of_fluid, float gravitational_acceleration);
+	
+	float specific_weight(float fluid_density, float gravitational_acceleration);
+	
+	//Useful for compressible fluids.
+	float cauchy_number(float fluid_density, float flow_velocity, float bulk_modulus_elasticity);
 }
