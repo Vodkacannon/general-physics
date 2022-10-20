d8dd0a20e7c1575631fdb5545328a336d353ebe7
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-20 14:47:15 -0400

    Update double_waves.cpp

--- src/double_waves.cpp
+++ src/double_waves.cpp
@@ -1,1 +1,27 @@
+#include "../include/constants.hpp"
 
+namespace simple_physics
+{
+  	double wave_intensity(double power, double area)
+	{
+		return power / area;
+	}
+	double index_of_refraction(double speed_of_light_in_medium)
+	{
+		return SPEED_OF_LIGHT / speed_of_light_in_medium;
+	}
+	
+	double wave_velocity(double frequency, double wavelength)
+	{
+		return frequency * wavelength;
+	}
+	double doppler_effect(double sound_wave_speed, double observer_velocity, double source_velocity, double origional_sound_wave_frequency)
+	{
+		return ((sound_wave_speed + observer_velocity) / (sound_wave_speed + source_velocity)) * origional_sound_wave_frequency;
+	}
+	
+	double waves_per_second(double wave_velocity, double wave_width) 
+	{
+		return wave_velocity / wave_width;
+	}
+}
