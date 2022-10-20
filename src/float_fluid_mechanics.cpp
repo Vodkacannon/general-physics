d44eb460a7f325e003ce0934027cd5d0dec0461e
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-20 16:09:16 -0400

    Update float_fluid_mechanics.cpp

--- src/float_fluid_mechanics.cpp
+++ src/float_fluid_mechanics.cpp
@@ -1,4 +1,5 @@
-
+#include <cmath>
+#include "../include/float_fluid_mechanics.h"
 
 namespace general_physics
 {
@@ -10,5 +11,11 @@ namespace general_physics
 	float bouyant_weight(float fluid_density, float body_immersion_volume, float gravitational_acceleration, float actual_weight)
 	{
 		return actual_weight - bouyant_force(fluid_density, body_immersion_volume, gravitational_acceleration);
+	}
+	
+	//Pipe volume flow rate.
+	float hagen_poiseuille_law(float pressure_difference, float pipe_radius, float fluid_viscosity, float pipe_length)
+	{
+		return (M_PI * pressure_difference * pipe_radius * powf(pipe_radius, 4.0f)) / (8.0f * fluid_viscosity * pipe_length);
 	}
 }
