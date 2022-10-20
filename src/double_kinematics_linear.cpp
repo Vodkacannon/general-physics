5a6889902694dde051b13a0d524166883609372e
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-20 14:56:09 -0400

    Update double_kinematics_linear.cpp

--- src/double_kinematics_linear.cpp
+++ src/double_kinematics_linear.cpp
@@ -29,12 +29,12 @@ namespace general_physics
 
   double displacement_2(double velocity_1, double time, double acceleration)
   {
-    return velocity_1 * time + (1 / 2) * acceleration * time * time;
+    return velocity_1 * time + 0.5 * acceleration * time * time;
   }
 
   double displacement_3(double velocity_1, double velocity_2, double acceleration)
   {
-    return (velocity_2 * velocity_2 - velocity_1 * velocity_1) / (2 * acceleration);
+    return (velocity_2 * velocity_2 - velocity_1 * velocity_1) / (2.0 * acceleration);
   }
 
   double acceleration(double velocity_2, double velocity_1, double time_2, double time_1)
