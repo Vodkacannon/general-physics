1801283bfb93b01ba1213b67ec2503de6e6d3409
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-20 15:57:06 -0400

    Create float_fluid_mechanics.cpp

---
+++ src/float_fluid_mechanics.cpp
@@ -0,0 +1,14 @@
+
+
+namespace general_physics
+{
+	float bouyant_force(float fluid_density, float body_immersion_volume, float gravitational_acceleration) 
+	{
+		return fluid_density * body_immersion_volume * gravitational_acceleration;
+	}
+
+	float bouyant_weight(float fluid_density, float body_immersion_volume, float gravitational_acceleration, float actual_weight)
+	{
+		return actual_weight - bouyant_force(fluid_density, body_immersion_volume, gravitational_acceleration);
+	}
+}
