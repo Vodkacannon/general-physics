785762a699c3c2bd14ad7f2af8a8d148bb7cf790
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-20 16:21:49 -0400

    Update float_fluid_mechanics.h

--- src/include/float_fluid_mechanics.h
+++ src/include/float_fluid_mechanics.h
@@ -2,4 +2,7 @@ namespace general_physics
 {
         float bouyant_force(float fluid_density, float body_immersion_volume, float gravitational_acceleration);
         float bouyant_weight(float fluid_density, float body_immersion_volume, float gravitational_acceleration, float actual_weight);
+        
+        //Pipe volume flow rate.
+	float hagen_poiseuille_law(float pressure_difference, float pipe_radius, float fluid_viscosity, float pipe_length);
 }
