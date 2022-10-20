05d785e3d0b2fcd42783ff961a0a3bee106b03b3
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-20 15:56:02 -0400

    Update and rename float_fluid_mechanics.cpp to float_fluid_mechanics.hpp

---
+++ src/include/float_fluid_mechanics.hpp
@@ -0,0 +1,7 @@
+#include "../include/float_fluid_mechanics.h"
+
+namespace general_physics
+{
+        float bouyant_force(float fluid_density, float body_immersion_volume, float gravitational_acceleration);
+        float bouyant_weight(float fluid_density, float body_immersion_volume, float gravitational_acceleration, float actual_weight);
+}
