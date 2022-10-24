af900cdcc8c47e1cc5f2bb1a4bd1caf75926be64
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2022-10-23 20:37:17 -0400

    Update float_misc.hpp

--- src/include/float_misc.hpp
+++ src/include/float_misc.hpp
@@ -4,9 +4,9 @@
 namespace general_physics
 {
 	/*
-Calculate the number of significant figures of a float.
-This function is not fully accurate!
-*/
+	Calculate the number of significant figures of a float.
+	This function is not fully accurate!
+	*/
 	float significant_figures(float value);
 	
 	float sum_of_momenta(const std::vector<std::pair<float_types::mass, float_types::velocity>>& momenta);
@@ -24,5 +24,11 @@ This function is not fully accurate!
 	//Energy to separate (Between two plates).
 	float casimir_energy_2_plates(float area_of_plate_front, float distance_between_plates);
 	
-	float lorentz_force(float charge, float velocity, float magnetic_field_strength, float angle_between_magnetic_field_and_velocity);	
+	float lorentz_force(float charge, float velocity, float magnetic_field_strength, float angle_between_magnetic_field_and_velocity);
+	
+	float magnetic_force(float particle_charge, float particle_velocity, magnetic_field, float angle_between_velocity_and_field);
+	
+	float magnetic_flow_meter_induced_voltage(float magnetic_field, float distance_between_electrodes, float liquid_velocity);
+	
+	float magnetic_h_field(float magnetic_field, float magnetization_vector);
 }
